406fb543eac88e49303f2ac3b517da49fc8747ea
bala-amypo <bala@amypo.in>
2025-12-15 17:23:50 +0000

    User commit

--- src/main/java/com/example/demo/entity/CourseCompletion.java
+++ src/main/java/com/example/demo/entity/CourseCompletion.java
@@ -9,4 +9,53 @@ public class CourseCompletion {
     private LocalDate completionDate;
     private String completionStatus;
     private double scorePercentage;
+    public CourseCompletion(){}
+    public CourseCompletion(User user, Course course, LocalDate completionDate, String completionStatus,
+            double scorePercentage) {
+        this.user = user;
+        this.course = course;
+        this.completionDate = completionDate;
+        this.completionStatus = completionStatus;
+        this.scorePercentage = scorePercentage;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+    public void setCompletionDate(LocalDate completionDate) {
+        this.completionDate = completionDate;
+    }
+    public void setCompletionStatus(String completionStatus) {
+        this.completionStatus = completionStatus;
+    }
+    public void setScorePercentage(double scorePercentage) {
+        this.scorePercentage = scorePercentage;
+    }
+    public long getId() {
+        return id;
+    }
+    public User getUser() {
+        return user;
+    }
+    public Course getCourse() {
+        return course;
+    }
+    public LocalDate getCompletionDate() {
+        return completionDate;
+    }
+    public String getCompletionStatus() {
+        return completionStatus;
+    }
+    public double getScorePercentage() {
+        return scorePercentage;
+    }
+
+
+
+    
 }
--- src/main/java/com/example/demo/entity/SkillGap.java
+++ src/main/java/com/example/demo/entity/SkillGap.java
@@ -1,5 +1,23 @@
 package com.example.demo.entity;
 
+import java.time.LocalDateTime;
+
 public class SkillGap {
+    private long id;
+    private User user;
+    private String skillName;
+    private String gapLevel;
+    private String suggestion;
+    private LocalDateTime generatedOn;
+    piblic SkillGap(){}
+    public SkillGap(User user, String skillName, String gapLevel, String suggestion, LocalDateTime generatedOn) {
+        this.user = user;
+        this.skillName = skillName;
+        this.gapLevel = gapLevel;
+        this.suggestion = suggestion;
+        this.generatedOn = generatedOn;
+    }
+    
+
     
 }
