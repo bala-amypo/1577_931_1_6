2c672afe2850d970fd4ec568b325f4d0c1901669
bala-amypo <bala@amypo.in>
2025-12-15 17:24:25 +0000

    User commit

--- src/main/java/com/example/demo/entity/SkillGap.java
+++ src/main/java/com/example/demo/entity/SkillGap.java
@@ -9,13 +9,49 @@ public class SkillGap {
     private String gapLevel;
     private String suggestion;
     private LocalDateTime generatedOn;
-    piblic SkillGap(){}
+    public SkillGap(){}
     public SkillGap(User user, String skillName, String gapLevel, String suggestion, LocalDateTime generatedOn) {
         this.user = user;
         this.skillName = skillName;
         this.gapLevel = gapLevel;
         this.suggestion = suggestion;
         this.generatedOn = generatedOn;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public void setSkillName(String skillName) {
+        this.skillName = skillName;
+    }
+    public void setGapLevel(String gapLevel) {
+        this.gapLevel = gapLevel;
+    }
+    public void setSuggestion(String suggestion) {
+        this.suggestion = suggestion;
+    }
+    public void setGeneratedOn(LocalDateTime generatedOn) {
+        this.generatedOn = generatedOn;
+    }
+    public long getId() {
+        return id;
+    }
+    public User getUser() {
+        return user;
+    }
+    public String getSkillName() {
+        return skillName;
+    }
+    public String getGapLevel() {
+        return gapLevel;
+    }
+    public String getSuggestion() {
+        return suggestion;
+    }
+    public LocalDateTime getGeneratedOn() {
+        return generatedOn;
     }
     
 
