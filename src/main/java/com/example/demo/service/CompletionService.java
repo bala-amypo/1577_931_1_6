3f635e6f6354459b957d07f5870f60874ca20b82
bala-amypo <bala@amypo.in>
2025-12-15 17:38:50 +0000

    User commit

--- src/main/java/com/example/demo/service/CompletionService.java
+++ src/main/java/com/example/demo/service/CompletionService.java
@@ -1,5 +1,11 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import com.example.demo.entity.CourseCompletion;
+
 public interface CompletionService {
-    
+    CourseCompletion logCompletetion(long userId,long courseId,double scorePercentage);
+    List<CourseCompletion> getCompletionsForUser(long userId);
+    double getSkillCoverageScore(long userId);
 }
--- src/main/java/com/example/demo/service/CourseService.java
+++ src/main/java/com/example/demo/service/CourseService.java
@@ -1,5 +1,11 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import com.example.demo.entity.Course;
+
 public interface CourseService {
-    
+    Course addCourse(Course course);
+    List<Course> getAllCourses();
+    String deleteCourse(long courseId);
 }
