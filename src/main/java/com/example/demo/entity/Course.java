7172ebed779aca25692ce79e7ff129e66a35b130
bala-amypo <bala@amypo.in>
2025-12-15 16:40:02 +0000

    User commit

--- src/main/java/com/example/demo/entity/Course.java
+++ src/main/java/com/example/demo/entity/Course.java
@@ -2,5 +2,17 @@ package com.example.demo.entity;
 
 public class Course {
     private long id;
-    private S
+    private String courseCode;
+    private String courseName;
+    private String category;
+    private String skillTags;
+    public Course(){}
+    public Course(String courseCode,String courseName,String category,String skillTags){
+        this.courseCode=courseCode;
+        this.courseName=courseName;
+        this.category=category;
+        this.skillTags=skillTags;
+
+    }
+    public void setId()
 }
